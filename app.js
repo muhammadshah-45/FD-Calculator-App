07e49f2cb6b01f6f8ccf839bec88e04be0ba3349
Muhammad Shah <135220760+muhammadshah-45@users.noreply.github.com>
2024-05-02 16:42:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function calculateAmount(){
+// Get input values from the form elements
+const principle=parseFloat(document.getElementById('principle').value);
+const interestRate=parseFloat(document.getElementById('interestRate').value);
+const tenure=parseFloat(document.getElementById('tenure').value);
+//Perform the Calculation
+const maturityAmount=principle*(principle*interestRate*tenure)/100;
+//Display the result
+document.getElementById('result').innerText=`Maturity Amount:${maturityAmount.toFixed(2)} `
+}
+// Attach the event listener to the calculate button
+document.getElementById('calculateBtn').addEventListener("click",calculateAmount);
